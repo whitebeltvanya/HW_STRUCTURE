1122480f8fe57557663ce37fc810ccfb89cc5f4d
vanya <whitebeltvanya@gmail.com>
2025-06-30 01:34:33 +0300

    first variant

--- Main.java
+++ Main.java
@@ -4,17 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите цену товара (в руб.):");
-        int priceRub = scanner.nextInt();
+        int priceRub, weightKg;
+        while (true) {
+            System.out.print("Введите цену товара (в руб.):");
+            priceRub = scanner.nextInt();
 
-        System.out.print("Введите вес товара (в кг.):");
-        int weightKg = scanner.nextInt();
+            System.out.print("Введите вес товара (в кг.):");
+            weightKg = scanner.nextInt();
 
-        if (priceRub >=0 && weightKg >=0)
-            System.out.println("ggg");
-        else
-            System.err.println("Неверно указан вес или цена товара!");
+            if (priceRub < 0 || weightKg < 0) {
+                System.out.println("Неверно указан вес или цена товара!");
+            }
+            else break;
+        }
 
+        System.out.print("Размер пошлины (в руб.) составит:" +calcTax(priceRub, weightKg));
+
+    }
+
+    //calc incoming tax
+    public static int calcTax (int priceRub, int weightKg){
+            return priceRub/100 + weightKg*100;
     }
 
 }
